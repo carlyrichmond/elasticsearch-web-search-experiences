1ec3b91155a2c2f0e3b6daf1bf1e9e25ea8db4d8
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-02-08 16:24:09 +0000

    Adding starting point web app

---
+++ starting-app/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../starting-app/index.html'));
+});
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+  console.log(`Recommending movies on port ${port}`);
+});
