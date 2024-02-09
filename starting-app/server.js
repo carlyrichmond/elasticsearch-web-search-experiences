c42d699d63ca6e914f578097c029d53e4323740b
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-02-09 17:09:53 +0000

    Correcting index.html path in root endpoint

--- starting-app/server.js
+++ starting-app/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../starting-app/index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.use(express.static(__dirname));
